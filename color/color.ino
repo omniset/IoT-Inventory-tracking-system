98e64f551ce3d1a9a9e0806e16da7ce68f34f885
MnD <95845768+omniset@users.noreply.github.com>
2023-05-20 09:32:36 +0700

    IoT_inventory_tracking.ino

--- color/color.ino
+++ color/color.ino
@@ -1,35 +1,38 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ThingSpeak.h>
-#include <Servo.h>
 #define SS_PIN 4  //D2
 #define RST_PIN 5 //D1
 #include <SPI.h>
 #include <MFRC522.h>
 
+//thingspeak dan koneksi wifi
 const char * myWriteAPIKey = "E5HT8NGK54DWY134";
 unsigned long myChannelNumber = 2141311;
 const char *ssid =  "Calistu Ionut Home";    
 const char *pass =  "MeRiCaNut2714"; 
 WiFiClient client;
-Servo dropServo;
+
+//pin sensor dan esp
 const int s0 = D4;
 const int s1 = D5;
 const int s2 = D6;  
 const int s3 = D7;  
 const int out = D8; 
-  
+
+//insialisasi 
 int red = 0;  
 int green = 0;  
 int blue = 0; 
 int color=0;
+
+//inisialisasi untuk thingspeak 
 int redcolor = 0;
 int greencolor = 0;
 int orangecolor = 0;
 int yellowcolor = 0;
 
-int pos = 0;
-
+//inisialisasi rfid
 MFRC522 mfrc522(SS_PIN, RST_PIN);   
 int variable = 0;
 
@@ -43,7 +46,6 @@ void setup()
   pinMode(out, INPUT);  
   digitalWrite(s0, HIGH);  
   digitalWrite(s1, HIGH);  
-  dropServo.attach(D1);
   
   Serial.begin(9600);   
   SPI.begin();      
@@ -80,25 +82,21 @@ void loop()
   Serial.print(" B Intensity : ");  
   Serial.print(blue, DEC);  
     if(red<85 & red>75 & green<80 & green>73 &blue<81 & blue>76){
-      dropServo.write(73);
       redcolor++;
       Serial.print("Red");
       ThingSpeak.writeField(myChannelNumber, 1,redcolor, myWriteAPIKey);
     }
     if(green<45 & green>40 & blue<55 &blue>48){
-      dropServo.write(107);
       orangecolor++;
       Serial.print("Orange");
       ThingSpeak.writeField(myChannelNumber, 2,orangecolor, myWriteAPIKey);
     }
     if(red<112 & red>100 & green<80 & green>63){
-      dropServo.write(132);
       greencolor++;
       Serial.print("Green");
       ThingSpeak.writeField(myChannelNumber, 3,greencolor, myWriteAPIKey);
     }
     if(red<35 & red>22 & green<33 & green>28 & blue<35 & blue>28){
-      dropServo.write(162);
       yellowcolor++;
       Serial.print("Yellow");
       ThingSpeak.writeField(myChannelNumber, 4,yellowcolor, myWriteAPIKey);
@@ -125,7 +123,7 @@ void loop()
      Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
+  }o
   content.toUpperCase();
   Serial.println();
   if (content.substring(1) == "C1 9A 15 44") 
@@ -141,13 +139,13 @@ void loop()
       Serial.println(" WARNA:YELLOW ");
       delay(300);
     }
-   else if (content.substring(1) == "")
+   else if (content.substring(1) == " ")
     {
       Serial.println(" KOTAK BUAH APEL ");
       Serial.println(" WARNA:ORANGE ");
       delay(300);
     }
-   else if (content.substring(1) == "") 
+   else if (content.substring(1) == " ") 
     {
       Serial.println(" KOTAK BUAH APEL ");
       Serial.println(" WARNA:GREEN ");
